4040815be34ee7f200610fcdcedaecfe141b0f0d
josefilho <iamjose.filho@gmail.com>
2021-05-11 10:01:06 -0400

    feat: Configurando store

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useSelector } from 'react-redux';
+
+const Catalog: React.FC = () => {
+  const store = useSelector(state => state);
+
+  console.log(store);
+  return (
+    <h1>Catalog</h1>
+  );
+}
+
+export default Catalog;
